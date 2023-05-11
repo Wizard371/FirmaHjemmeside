5d6e62331dabcf888790c29241954bc64a980346
Wizard371 <46074189+Wizard371@users.noreply.github.com>
2023-05-11 22:44:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
+// DROPDOWN 
 var toggle = document.querySelector('.dropdown-toggle');
 var content = document.querySelector('.dropdown-content');
 
-toggle.addEventListener('click', function() {
+function toggleDropdown() {
     content.classList.toggle('show');
+}
+
+// Show dropdown menu on click for mobile devices
+toggle.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevents the default behavior of the link/button
+    if (window.innerWidth <= 768) {
+        toggleDropdown();
+    }
+});
+
+// Show dropdown menu on hover for larger screens
+toggle.addEventListener('mouseenter', function() {
+    if (window.innerWidth > 768) {
+        toggleDropdown();
+    }
+});
+
+content.addEventListener('mouseenter', function() {
+    if (window.innerWidth > 768) {
+        toggleDropdown();
+    }
+});
+
+toggle.addEventListener('mouseleave', function() {
+    if (window.innerWidth > 768) {
+        toggleDropdown();
+    }
+});
+
+content.addEventListener('mouseleave', function() {
+    if (window.innerWidth > 768) {
+        toggleDropdown();
+    }
+});
+
+// Hide dropdown menu when window is resized to a larger size
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 768) {
+        content.classList.remove('show');
+    }
 });
 
 
+
+
+// ADD TO CART FUNKTION 
 
 // Find "add to cart" knapperne
 var addToCartButtons = document.querySelectorAll('.product button');
@@ -29,3 +73,5 @@ for (var i = 0; i < addToCartButtons.length; i++) {
         cartTotal.textContent = total + ' DKK';
     });
 }
+
+// DROPDOWN SAMME STØRELSE SOM KNAPPEN
